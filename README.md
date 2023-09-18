003d916b602785bd1eb6747a159cbcc91e3edb7f
Suuskehh <suus.harsveld@hva.nl>
2023-09-18 14:51:24 +0200

    Update art

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# css-art
-CSS art testing room
+> Fork deze leertaak en ga aan de slag. Onderstaande outline ga je gedurende deze taak in jouw eigen GitHub omgeving uitwerken. De instructie vind je in: [docs/INSTRUCTIONS.md](docs/INSTRUCTIONS.md)
+
---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){  
+
+
+$("#namebutton").click(function(){
+  $(".name") .hide();
+
+});
+
+$("#namebutton").click(function(){
+  $(".degree") .show();
+
+});
+
+$("#degreebutton").click(function(){
+  $(".degree") .hide();
+
+});
+
+$("#degreebutton").click(function(){
+  $(".photo") .show();
+
+});
+
+$("#photobutton").click(function(){
+  $(".photo") .hide();
+
+});
+
+$("#photobutton").click(function(){
+  $(".about") .show();
+
+});
+
+$("#aboutbutton").click(function(){
+  $(".about") .hide();
+
+});
+
+$("#aboutbutton").click(function(){
+  $(".socials") .show();
+
+});
+
+$("#socialsbutton").click(function(){
+  $(".socials") .hide();
+
+});
+
+$("#socialsbutton").click(function(){
+  $(".name") .show();
+
+});
+
+});
